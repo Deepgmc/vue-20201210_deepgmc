cf5804f218dc01db98e0d7c5038bd245b658e797
sergey <s.suhorukov@prcs.ru>
2020-12-27 15:09:40 +0300

    02 counter button done

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,31 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: `
+    <button
+      type="button"
+      @click="handleBtnClick"
+    >{{counter}}</button>
+  `,
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
 
   // Компонент должен иметь модель
+  data: function () {
+    return {
+      counter: this.count
+    }
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    handleBtnClick() {
+      this.$emit('increment', this.count + 1)
+    }
+  }
 };
